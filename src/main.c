a6a5319e6e9dbba0da63beb84ae088106a6f333e
Miranda Dodson <miranda.leih.dodson@gmail.com>
2019-11-09 14:51:52 -0600

    Added interrupt.

--- src/main.c
+++ src/main.c
@@ -6,22 +6,29 @@
 const int opto = 29;
 
 void init_wiringpi(void);
+void init_interrupt(void);
+void print_val(void);
 
 int main(void) {
     // printf("Hello World \n");
     init_wiringpi();
-
-    while(1)
-    {
-        int value = digitalRead(opto);
-        printf("The current values is: %d\n",value);
-    }
+    init_interrupt();
     return EXIT_SUCCESS;
 }
 
 void init_wiringpi(void) {
     wiringPiSetup();
     pinMode(opto,INPUT);
+
+}
+
+void init_interrupt(void) {
+    wiringPiISR(opto, INT_EDGE_FALLING, print_val);
+}
+
+void print_val(void){
+    int value = digitalRead(opto);
+    printf("The current values is: %d\n",value);
 
 }
 
